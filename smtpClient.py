27af3f7afe0fb662bf86c05875626189b6633eaf
Hunter Mickiewicz <hsm5680@nyu.edu>
2024-02-22 20:31:24 -0500

    Skelly code filled in

--- smtpClient.py
+++ smtpClient.py
@@ -10,6 +10,8 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
     # Create socket called clientSocket and establish a TCP connection with mailserver and port
 
     # Fill in start
+    clientSocket = socket(AF_INET, SOCK_STREAM)
+    clientSocket.connect((mailserver,port))
     # Fill in end
 
     recv = clientSocket.recv(1024).decode()
@@ -27,26 +29,52 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
 
     # Send MAIL FROM command and handle server response.
     # Fill in start
+    mailFromCommand = 'MAIL FROM: <hsm5680@nyu.edu>\r\n'
+    clientSocket.send(mailFromCommand.encode())
+    recv2 = clientSocket.recv(1024).decode()
+    #print(recv2)
     # Fill in end
 
     # Send RCPT TO command and handle server response.
     # Fill in start
+    rcptToCommand = 'RCPT TO: <hsm5680@nyu.edu>\r\n'
+    clientSocket.send(rcptToCommand.encode())
+    recv3 = clientSocket.recv(1024).decode()
+    #print(recv3)
     # Fill in end
 
     # Send DATA command and handle server response.
     # Fill in start
+    dataCommand = 'DATA\r\n'
+    clientSocket.send(dataCommand.encode())
+    recv4 = clientSocket.recv(1024).decode()
+    #print(recv4)
     # Fill in end
 
     # Send message data.
     # Fill in start
+    lineOne = 'I really like Python.\r\n'
+    clientSocket.send(lineOne.encode())
+    lineTwo = 'It makes programming assignments like this easier!\r\n'
+    clientSocket.send(lineTwo.encode())
+    lineThree = 'Can you imagine doing somehting like this in C? *shudder*\r\n'
+    clientSocket.send(lineThree.encode())
     # Fill in end
 
     # Message ends with a single period, send message end and handle server response.
     # Fill in start
+    lineFour = '.\r\n'
+    clientSocket.send(lineFour.encode())
+    recv5 = clientSocket.recv(1024).decode()
+    #print(recv5)
     # Fill in end
 
     # Send QUIT command and handle server response.
     # Fill in start
+    quitCommand = 'QUIT\r\n'
+    clientSocket.send(quitCommand.encode())
+    recv6 = clientSocket.recv(1024).decode()
+    #print(recv6)
     # Fill in end
 
 
